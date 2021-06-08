60b1877ff790ebd99bfb4517191e1ee9cd72f176
Kirill <kirillaromad@mail.ru>
2021-06-08 06:23:04 +0300

    app

---
+++ CreditApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
