b81a4a644e859ac44e5f7b0458836ba17947bf5b
Kirill <kirillaromad@mail.ru>
2021-06-08 10:13:25 +0300

    db

--- Models/CreditApplication.cs
+++ Models/CreditApplication.cs
@@ -1,4 +1,7 @@
-﻿namespace Models
+﻿using System.Linq;
+using DataBase;
+
+namespace Models
 {
     public class CreditApplication
     {
@@ -32,7 +35,14 @@
             Job = job;
             Deposit = deposit;
             CarAge = carAge;
+            
             // HasAnotherCredits проверяем в бд
+            var context = new ApplicationContext();
+            var creditInfo = context.CreditInfos
+                .Where(c => c.Surname == clientFullName.Surname)
+                .FirstOrDefault(c => c.Name == clientFullName.Name);
+            if (creditInfo != null)
+                HasAnotherCredits = true;
             
             var validator = new CreditApplicationValidator();
             ValidationResult = validator.Validate(this);
